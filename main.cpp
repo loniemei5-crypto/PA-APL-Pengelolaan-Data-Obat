ebf862149bab2301351388704a865688a4c27641
geooo14 <geooo140607@gmail.com>
2026-05-08 15:38:02 +0800

    fitur register

--- main.cpp
+++ main.cpp
@@ -23,6 +23,52 @@ void SaveJSON(string filename, json j)
     o << j.dump(4) << endl;
 }
 
+void setColor(int color)
+{
+    SetConsoleTextAttribute(
+        GetStdHandle(STD_OUTPUT_HANDLE),
+        color);
+}
+
+void registerUser(json &users)
+{
+    string username, password;
+    setColor(9);
+    cout << "\n===== REGISTER =====\n";
+    setColor(7);
+
+    setColor(3);
+    cout << "Enter username: ";
+    setColor(7);
+    cin >> username;
+
+    for (auto &u : users["users"])
+    {
+        if (u["username"] == username)
+        {
+            cout << "Username telah digunakan!\n";
+            return;
+        }
+    }
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    json u;
+
+    u["username"] = username;
+    u["password"] = password;
+
+    // default role register = kasir
+    u["role"] = "kasir";
+
+    users["users"].push_back(u);
+
+    void saveJSON("users.json", users);
+
+    cout << "Registrasi berhasil!\n";
+}
+
 int main()
 {
     cout << "Hello, World!" << endl;
